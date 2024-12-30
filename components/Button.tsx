3f84e61a8aaad38ccab3da320928c69caf3b9d46
Ali Hosseini <itassistant.2020@gmail.com>
2024-12-30 21:23:28 +0330

    fix-Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
 
 const Button = ({type , title , icon , variant , full}: ButtonProps) => {
   return (
-    <button className={`flex justify-center gap-3 rounded-full border ${variant} ${full && 'w-full'}`} type={type}>
+    <button className={`flex justify-center items-center gap-3 rounded-full border ${variant} ${full && 'w-full'}`} type={type}>
         {icon && <Image src={icon} alt={title} width={24} height={24} />}
         <label className="font-bold text-base whitespace-normal flex-nowrap">{title}</label>
     </button>
